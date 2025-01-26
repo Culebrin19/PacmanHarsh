db845ad31770fe58ea3b00c3ca4961309f13af23
Harsh Kumar Saini <150950663+Culebrin19@users.noreply.github.com>
2025-01-26 14:17:07 +0100

    food

--- js/sketch.js
+++ js/sketch.js
@@ -1,27 +1,40 @@
 import { GameObject } from "./classes/GameObject.js";
 
 const map = [
-  [1, 1, 1, 1],
-  [1, 0, 0, 1],
-  [1, 0, 0, 1],
-  [1, 1, 1, 1]
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0, 1],
+  [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 2, 0, 2, 0, 2, 0, 0, 2, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
-const ROWS = 4;
-const COLUMNS = 4;
+const ROWS = 14;
+const COLUMNS = 14;
 export const IMAGE_SIZE = 32;
-const WIDTH_CANVAS = IMAGE_SIZE * COLUMNS;
-const HEIGHT_CANVAS = IMAGE_SIZE * ROWS;
+const WIDTH_CANVAS = 448;
+const HEIGHT_CANVAS = 448; // IMAGE_SIZE * ROWS
 
 let imgRock;
+let imgFood;
 
 const arrRocks = [];
+const arrFood = [];
 const numberImagesLoaded = 0;
 console.log("Boff");
 
 function preload() {
   console.log("o");
   imgRock = loadImage("../img/roca.png", handleImage, handleError);
+  imgFood = loadImage("../img/food.png", handleImage, handleError);
 }
 
 function handleError() {
@@ -41,6 +54,10 @@ function setup() {
         const roca = new GameObject(filaActual, columnaActual);
         console.log(`He creat roca a posicio fila ${ROWS}i columna ${COLUMNS}`);
         arrRocks.push(roca);
+      } else if (map[filaActual][columnaActual] === 2) {
+        const menjar = new GameObject(filaActual, columnaActual);
+        console.log(`He creat roca a posicio fila ${ROWS}i columna ${COLUMNS}`);
+        arrFood.push(menjar);
       }
     }
     console.log(arrRocks.length);
@@ -50,6 +67,7 @@ function setup() {
 function draw() {
   background(171, 248, 168);
   arrRocks.forEach((roca) => roca.showObject(imgRock));
+  arrFood.forEach((menjar) => menjar.showObject(imgFood));
 }
 
 globalThis.setup = setup;
